2f55e6184aeca222867afaa9ed1e4c592d4e9a61
Witcier <770201276@qq.com>
2021-05-13 15:50:10 +0800

    Service 开发准备

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('v1')
+    ->namespace('Api/V1')
+    ->name('api.v1.')
+    ->group(function() {
+        Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function () {
+
+        });
+
+        Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
+
+            // 登录之后可以访问的接口
+            Route::middleware('auth:api')->group(function () {
+
+            });
+        });
+});
